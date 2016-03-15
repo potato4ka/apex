564dc2c611f72a645ded809420b463f58ff48bbb
Tania Butova <tato4ka-potato4ka@yandex.ru>
2016-03-16 01:31:34 +0200

    final styles. Dropdown.

--- apex/content/js/main.js
+++ apex/content/js/main.js
@@ -4,9 +4,18 @@
         e.preventDefault()
     });
     $('.aside').height($('.main-wrapper').height() - 2);
+    $('.nav-hamburger a').click(function (e) {
+        $(".drop-list").slideToggle();
+        e.preventDefault()
+    })
 });
 
 $( window ).resize(function(){
     $('.aside').height('auto');
     $('.aside').height($('.main-wrapper').height() - 1);
+    if ($(window).width()> 740){
+        $(".drop-list").show();
+    }else {
+        $(".drop-list").hide();
+    }
 });
