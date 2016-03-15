e73ef6018c3fe2eb0b84f33a9cadaaf984cb1c58
Tania Butova <tato4ka-potato4ka@yandex.ru>
2016-03-16 00:42:25 +0200

    global fixes

--- apex/content/js/main.js
+++ apex/content/js/main.js
@@ -1,8 +1,12 @@
-﻿$(function () {
+﻿$(document).ready(function() {
     $(".lnk-opener").click(function (e) {
         $(".container").toggleClass("opened");
         e.preventDefault()
     });
-    $('.aside').height($('.main-wrapper').height() - $('.header').height());
+    $('.aside').height($('.main-wrapper').height() - 2);
 });
 
+$( window ).resize(function(){
+    $('.aside').height('auto');
+    $('.aside').height($('.main-wrapper').height() - 1);
+});
