830ab424249582b2a082f46bf27b9e00fe736026
Tania Butova <tato4ka-potato4ka@yandex.ru>
2016-03-15 19:06:06 +0200

    initial commit

---
+++ apex/Content/JS/JavaScript.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+
+    $
+    ("#menu-toggle").click(function (e) {
+        $("#wrapper").toggleClass("toggled");
+        e.preventDefault()
+    });
+    $(".btn-instance").click(function (e) {
+        $("#wrapper").addClass("toggled");
+        e.preventDefault()
+    });
+    $(".lnk-back").click(function (e) {
+        $("#wrapper").removeClass("toggled");
+        e.preventDefault()
+    });
+    $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+
+    $("#menu-icon").on("click", function () {
+        $("nav").slideToggle("normal");
+        $(this).toggleClass("active");
+    });
+});
+
